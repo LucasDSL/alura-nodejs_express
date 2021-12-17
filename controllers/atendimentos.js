ea4e30e5218a5dc721225126513b41e749eab49d
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2021-12-17 15:15:42 -0300

    one more route && config on customExpress

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => {
   app.get("/atendimentos", (req, res) =>
     res.send("Server running - appointments")
   )
+
+  app.post("/atendimentos", (req, res) => {
+    console.log(req.body)
+    res.send("Dados recebidos")
+  })
 }
