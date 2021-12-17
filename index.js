00dc1f35c72e5039ac17f9a01145b4072e2e2760
Lucas Lima <67974468+LucasDSL@users.noreply.github.com>
2021-12-17 15:33:21 -0300

    Creating table Atendimentos when starting server and connecting to database

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const customExpress = require("./config/customExpress")
 const app = customExpress()
+const conexao = require("./infraestrutura/conexao")
+const Tabelas = require("./infraestrutura/tabelas")
 
-app.listen(3000, () => {
-  console.log("Listening port 3000")
+conexao.connect(err => {
+  if (err) {
+    console.log(err)
+  } else {
+    Tabelas.init(conexao)
+    app.listen(3000, () => {
+      console.log("Listening port 3000")
+    })
+  }
 })
---
+++ infraestrutura/tabelas.js
@@ -0,0 +1,20 @@
+class Tabelas {
+  init(conexao) {
+    this.conexao = conexao
+    this.criarAtendimento()
+  }
+
+  criarAtendimento() {
+    const sql =
+      "CREATE TABLE Atendimentos (id int NOT NULL AUTO_INCREMENT, Cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
+    this.conexao.query(sql, err => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log("Tabela atendimentos criada com sucesso!")
+      }
+    })
+  }
+}
+
+module.exports = new Tabelas()
